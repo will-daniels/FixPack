b0868353e08c40fec404fa90234180f77e27b4f8
Will "Rents" Daniels <willdaniels@nas-10-240-246-166.york.ac.uk>
2020-02-14 14:53:17 +0000

    Initial commit

---
+++ app/src/main/java/com/example/fixpack/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.fixpack;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* Dynamic Gradient Background */
+        ConstraintLayout constraintLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(200);
+        animationDrawable.setExitFadeDuration(8000);
+        animationDrawable.start();
+    }
+}
